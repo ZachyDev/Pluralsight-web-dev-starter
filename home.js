1ec6d513c85591a0b1adb3fd3d35fc15898def1b
ZachyDev <mosetizachary001@gmail.com>
2021-06-03 14:59:21 +0300

    Working with JavaScript files

--- home.js
+++ home.js
@@ -1,2 +1,1 @@
-
-showMessage('help');
+showMessage('Changes...')
--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
-function showMessage(message) {
+const showMessage = message => {
     document.getElementById('message').textContent = message;
 }
